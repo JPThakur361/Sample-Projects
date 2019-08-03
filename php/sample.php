22adeb0edca916d40a062625fa230b623b4292e2
JPThakur361 <js1081996@gmail.com>
2019-08-03 19:36:53 +0530

    new

--- php/sample.php
+++
@@ -1,7 +0,0 @@
-
-<script>if(confirm('Are you sure you want to Logout?'))
-{window.location='Logout.php';
-}else{
-window.location='gallery.php';
-}
-</script>
--- php/updatecall.php
+++
@@ -1,8 +0,0 @@
-
-<script>if(confirm('Would you like to reserve this movie?'))
-{
-window.location='Login.php';
-}else{
-window.location='UPDATE.PHP';
-}
-</script>
